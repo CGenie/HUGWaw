8857219df0cd95898826c2674f9181fc2ba2c909
Przemek Kaminski <cgenie@gmail.com>
2013-06-04 17:17:42 +0000

    YESOD REST: Echo working

--- meetings/2013-06-06/presentation/rest/Handler/Echo.hs
+++ meetings/2013-06-06/presentation/rest/Handler/Echo.hs
@@ -3,4 +3,4 @@ module Handler.Echo where
 import Import
 
 getEchoR :: String -> Handler Html
-getEchoR = error "Not yet implemented: getEchoR"
+getEchoR theText = defaultLayout [whamlet|<h1>#{theText}|]

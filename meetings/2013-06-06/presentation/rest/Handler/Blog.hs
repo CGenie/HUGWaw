1ddde8d188803433d01c90ef79a1f4651e21cf7b
Przemek Kaminski <cgenie@gmail.com>
2013-06-04 20:16:46 +0000

    YESOD REST: Blog & Article handlers

---
+++ meetings/2013-06-06/presentation/rest/Handler/Blog.hs
@@ -0,0 +1,38 @@
+module Handler.Blog (
+       getBlogR
+      ,postBlogR
+) where
+
+import Import
+
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+-- |najlepiej dac ta linijke w Foundation.hs
+instance YesodNic App
+
+-- |form do dodawania nowego artykulu
+-- areq - wymagany input dla forma
+-- areq <type> <label> <default_value>
+entryForm :: Form Article
+entryForm = renderDivs $ Article
+          <$> areq textField "Title" Nothing
+          <*> areq nicHtmlField "Content" Nothing
+
+getBlogR :: Handler Html
+getBlogR = do
+         -- |lista artykulow z DB
+         articles <- runDB $ selectList [] [Desc ArticleTitle]
+         (articleWidget, enctype) <- generateFormPost entryForm
+         defaultLayout $ do
+                         $(widgetFile "articles")
+
+postBlogR :: Handler Html
+postBlogR = do
+              ((res, articleWidget), enctype) <- runFormPost entryForm
+              case res of
+                   FormSuccess article -> do
+                                       articleId <- runDB $ insert article
+                                       setMessage $ toHtml $ (articleTitle article) <> "created"
+                                       redirect $ ArticleR articleId
+                   _                   -> defaultLayout $ do
+                                               setTitle "Please correct your entry form"
+                                               $(widgetFile "articleError")
